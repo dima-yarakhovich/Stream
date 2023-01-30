06d5db20a28d9513f168facb9a499d1c150b6674
dima-yarakhovich <dima020490@tut.by>
2023-01-30 22:29:01 +0300

    Initial commit

---
+++ src/test/java/com/skypro/stream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
